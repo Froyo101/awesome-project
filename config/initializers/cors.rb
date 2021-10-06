4479bca0b335554313e77e062900a8ab3fe0df4e
Froyo101 <jakehasmail@yahoo.com>
2021-10-06 14:49:16 -0500

    Force ssl true in prod environment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   #end
 
   # PRODUCTION - CHANGE LATER!
+  # NEED TO ALLOW MORE ORIGINS IF YOU WANT AUTH TO WORK W/ NON-HTTPS
   allow do
     origins "https://awesome-project-jf.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
